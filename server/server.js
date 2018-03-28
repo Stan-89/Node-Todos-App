af6765ffa865a330236a114bc46893641273d3dd
Stan <stanislavvitekov@gmail.com>
2018-03-28 09:25:00 -0400

    Post and create

--- server/server.js
+++ server/server.js
@@ -1,39 +1,45 @@
-//Import mongoose, which is already connected to the database.
-//Recal ES6 destructuring (instead of theObject.mongoose, straight up {mongoose}
-//so we declare it as a variable as well.
+//We're going to use express and bodyParser
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//Import mongoose, it will connect us to our db
 var {mongoose} = require('./db/mongoose.js');
 
-//Models importation
+//Models import
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
+//Our app
+var app = express();
 
-//Todo entity
-var newTodo = new Todo({
-  text: 'Feed the cats',
-  completed: false,
-  completedAt: 123123
+//Recall using middleware
+//We'll need this since incoming post will be a json
+app.use(bodyParser.json());
+
+//When we POST to todos
+app.post('/todos', (req, res) => {
+
+  //Note: no need for "manual" validation, since that's done in the model already
+  //by enforcing the required, types, etc. So we can straight up let the post
+
+  //Create todo instance
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  //After it is created, let's save it
+  //Recall that .save method on a Mongoose model entity is async
+  todo.save().then((doc) => {
+    //Save it in the response so we answer by this
+    //Status automatically OK
+    res.send(doc);
+  }, (e) => {
+    //But change the status to 400 since error
+    res.status(400).send(e);
+  });
+
 });
 
-
-//Insert a user entity
-var user = new User({
-  email: 'test@somewhere.ca'
+app.listen(3000, () => {
+  console.log("Started on port 3000");
 });
-
-user.save().then((doc) => {
-  console.log("Saved the user");
-}, (e) => {
-  console.log("Unable to save the todo", e);
-});
-
-
-//Save the stuff
-newTodo.save().then((doc) => {
-  console.log("Saved todo", doc);
-  mongoose.connection.close();
-}, (e) => {
-  console.log("Unable to save todo");
-});
-
-//Note:we put the close here, we get an error since insert hasn't finished yet
